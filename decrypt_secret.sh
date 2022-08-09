c161f644dea661ef0a8542c56054a852cf63c3d3
Vranda2088 <62051031+Vranda2088@users.noreply.github.com>
2022-08-09 18:32:20 +0530

    Update decrypt_secret.sh

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -5,4 +5,3 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" --output $HOME/secrets/config.json config.json.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$PK" --output $HOME/secrets/private.key private.key.gpg
